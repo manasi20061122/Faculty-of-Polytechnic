8c4bec8800346550d161c1902e6e0134bf32e980
sawantd581-wq <sawantd581@gmail.com>
2026-06-05 11:54:28 +0530

    Update Prachi-f2.py

--- students/prachi-savant/Prachi-f2.py
+++ students/prachi-savant/Prachi-f2.py
@@ -1,7 +1,7 @@
 #problem1
 s = {'a', 'b', 'c', 'd'}
 
-s.update({'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm',
+s.update({'e', 'f','g' 'h', 'i', 'j', 'k', 'l', 'm',
           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
           'w', 'x', 'y', 'z'})
 
